87be3ab5e2ed24006494f56262dbeb9f71e34d52
nginnever <ginneversource@gmail.com>
2021-07-04 17:37:25 -0700

    add owners

--- test/safeTest.ts
+++ test/safeTest.ts
@@ -1,8 +1,9 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
 import { expect } from 'chai'
 import { BigNumber, Contract } from 'ethers'
-import { ethers, network } from 'hardhat'
+import { ethers, network, waffle } from 'hardhat'
 import { DAOFixture, getFixtureWithParams } from './shared/fixtures'
+import { executeContractCallWithSigners } from './shared/utils'
 import { keccak256 } from 'ethereumjs-util'
 import { defaultSender, provider, web3, contract } from '@openzeppelin/test-environment';
 
@@ -17,13 +18,14 @@ let wallet: SignerWithAddress
 // - figure out how to get expect reverts working
 
 describe('houseDAOnft:', () => {
+  const [user1, user2, user3] = waffle.provider.getWallets();
 
   beforeEach(async function () {
     wallet = (await ethers.getSigners())[0]
     daoFixture = await getFixtureWithParams(wallet, true)
   })
 
-  it.only('house dao is initialized', async () => {
+  it('gnosis safe is initialized', async () => {
   	let wallet_1 = (await ethers.getSigners())[0]
     const { safe } = daoFixture
 
@@ -33,5 +35,18 @@ describe('houseDAOnft:', () => {
     console.log(wallet.address)
     console.log(owners)
     console.log(safe.address)
+  })
+
+  it.only('gnosis safe add owner', async () => {
+    let wallet_1 = (await ethers.getSigners())[0]
+    let wallet_2 = (await ethers.getSigners())[1]
+    const { safe } = daoFixture
+
+    let owners = await safe.getOwners()
+    console.log(owners)
+    //await safe.addOwnerWithThreshold(wallet_2.address, 1)
+    await executeContractCallWithSigners(safe, safe, "addOwnerWithThreshold", [user2.address, 1], [user1])
+    owners = await safe.getOwners()
+    console.log(owners)
   })
 })
