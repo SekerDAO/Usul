c0edcb37e6877fb53d1004a68dac06c72b311335
nginnever <ginneversource@gmail.com>
2021-12-01 08:35:10 -0800

    audit fix #16: simplify member quorum

--- contracts/votingStrategies/SimpleMemberVoting.sol
+++ contracts/votingStrategies/SimpleMemberVoting.sol
@@ -61,22 +61,6 @@ contract SimpleMemberVoting is BaseTokenVoting, BaseMember, BaseQuorumPercent {
         emit StrategySetup(_UsulModule, _owner);
     }
 
-    function addMember(address member) public override onlyOwner {
-        require(members[member] == false, "member is already registered");
-        members[member] = true;
-        _writeCheckpoint(_totalMemberCheckpoints, _add, 1);
-        memberCount++;
-        emit MemberAdded(member);
-    }
-
-    function removeMember(address member) public override onlyOwner {
-        require(members[member] == true, "member is not registered");
-        members[member] = false;
-        _writeCheckpoint(_totalMemberCheckpoints, _subtract, 1);
-        memberCount--;
-        emit MemberRemoved(member);
-    }
-
     /// @dev Determines if a proposal has succeeded.
     /// @param proposalId the proposal to vote for.
     /// @return boolean.
@@ -88,7 +72,7 @@ contract SimpleMemberVoting is BaseTokenVoting, BaseMember, BaseQuorumPercent {
         require(
             proposals[proposalId].yesVotes +
                 proposals[proposalId].abstainVotes >=
-                quorum(proposals[proposalId].startBlock),
+                quorum(block.number),
             "a quorum has not been reached for the proposal"
         );
         require(
@@ -105,87 +89,8 @@ contract SimpleMemberVoting is BaseTokenVoting, BaseMember, BaseQuorumPercent {
         returns (uint256)
     {
         return
-            (getPastTotalMembers(blockNumber) * quorumNumerator()) /
+            (memberCount * quorumNumerator()) /
             quorumDenominator();
-    }
-
-    function _writeCheckpoint(
-        Checkpoint[] storage ckpts,
-        function(uint256, uint256) view returns (uint256) op,
-        uint256 delta
-    ) private returns (uint256 oldWeight, uint256 newWeight) {
-        uint256 pos = ckpts.length;
-        oldWeight = pos == 0 ? 0 : ckpts[pos - 1].members;
-        newWeight = op(oldWeight, delta);
-
-        if (pos > 0 && ckpts[pos - 1].fromBlock == block.number) {
-            ckpts[pos - 1].members = newWeight;
-        } else {
-            ckpts.push(
-                Checkpoint({
-                    fromBlock: SafeCast.toUint32(block.number),
-                    members: newWeight
-                })
-            );
-        }
-    }
-
-    /**
-     * @dev Retrieve the `totalSupply` at the end of `blockNumber`. Note, this value is the sum of all balances.
-     * It is but NOT the sum of all the delegated votes!
-     *
-     * Requirements:
-     *
-     * - `blockNumber` must have been already mined
-     */
-    function getPastTotalMembers(uint256 blockNumber)
-        public
-        view
-        returns (uint256)
-    {
-        require(blockNumber < block.number, "ERC20Votes: block not yet mined");
-        return _checkpointsLookup(_totalMemberCheckpoints, blockNumber);
-    }
-
-    /**
-     * @dev Lookup a value in a list of (sorted) checkpoints.
-     */
-    function _checkpointsLookup(Checkpoint[] storage ckpts, uint256 blockNumber)
-        private
-        view
-        returns (uint256)
-    {
-        // We run a binary search to look for the earliest checkpoint taken after `blockNumber`.
-        //
-        // During the loop, the index of the wanted checkpoint remains in the range [low-1, high).
-        // With each iteration, either `low` or `high` is moved towards the middle of the range to maintain the invariant.
-        // - If the middle checkpoint is after `blockNumber`, we look in [low, mid)
-        // - If the middle checkpoint is before or equal to `blockNumber`, we look in [mid+1, high)
-        // Once we reach a single value (when low == high), we've found the right checkpoint at the index high-1, if not
-        // out of bounds (in which case we're looking too far in the past and the result is 0).
-        // Note that if the latest checkpoint available is exactly for `blockNumber`, we end up with an index that is
-        // past the end of the array, so we technically don't find a checkpoint after `blockNumber`, but it works out
-        // the same.
-        uint256 high = ckpts.length;
-        uint256 low = 0;
-        while (low < high) {
-            uint256 mid = Math.average(low, high);
-            if (ckpts[mid].fromBlock > blockNumber) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return high == 0 ? 0 : ckpts[high - 1].members;
-    }
-
-    function _add(uint256 a, uint256 b) private pure returns (uint256) {
-        return a + b;
-    }
-
-    function _subtract(uint256 a, uint256 b) private pure returns (uint256) {
-        return a - b;
     }
 
     function calculateWeight(address voter, uint256 proposalId)
